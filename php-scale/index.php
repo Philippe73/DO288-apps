55a9c13f5ec4900abf8f2e55a5cdcb3f69c661af
Student User <student@workstation.lab.example.com>
2020-11-12 11:25:03 -0500

    vvv

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,7 +1,3 @@
 <?php
-<<<<<<< HEAD
-  print "This is version 5 of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
-=======
   print "This is version 2 of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
->>>>>>> 42b5cb1ee93f997d68288fb2a8d03b8318e12fbd
 ?>
